f5731c48a354b051ac330b9e38f050260f5f84f9
Alexander Epolite <alexander@epolite.net>
2022-09-17 23:41:48 -0700

    reduce the loading time for index.html

--- js/index.js
+++ js/index.js
@@ -265,3 +265,12 @@ function regenerate() {
         location.reload();
     }
 }
+
+function topnav_resize() {
+    const x = document.getElementById("topnav");
+    if(x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
