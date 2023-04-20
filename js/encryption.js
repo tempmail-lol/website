71fb6e890178592eaacb4db41f10c610f34feeea
Alexander Epolite <alexander@epolite.net>
2023-04-19 23:44:44 -0700

    Upgrade by mail

---
+++ js/encryption.js
@@ -0,0 +1,60 @@
+
+const pubkey = "-----BEGIN PGP PUBLIC KEY BLOCK-----\n" +
+    "\n" +
+    "mQGNBGQ8LKkBDADZZIlMWvlDilDANfGrKdw2bR6sifs6gbiLKvqAd6k926ojVyBY\n" +
+    "+MTlfe7tAsB4drVBSGBrrm2SoCveOsAAxcgXN0pffKku2YyS0jRlyz+wJVKOw2BP\n" +
+    "YYUiHrky0olzPBniPn3/IfKEbfb382whytPYQPg+adI5scGwaSM+4uD8hP+1bssT\n" +
+    "UWHBOfBy+P+Led0hZRFO5GJiMVPC6ZYfXdYd7N1JbNHSB5RaGCBHxL3d1giQj1qZ\n" +
+    "d4D0eb+fIBmlPHOuchiXU9qOLzyACOaHfGn3i1/rEI0K1QlRjiVl9QhoaUfhNDH+\n" +
+    "PxK+gJI3GQcH9sA2vHzm9wHvDB3Om1+fR7O19vDNjnoRME6PeuVFipb1ilWM1Wur\n" +
+    "l15thYHQvnSNjJ9rRCHlZUsX/eGShmQkVbFHmQK7jOH2tmX0mktebX0JX0vGWk2G\n" +
+    "C1omea/MepNO/BasG+tSkAiXzusp8iz6fYgfBaOJeCKHIAgmk5+2vswCC95JUb5S\n" +
+    "eiK7gU+J/N4ds0sAEQEAAbQqQmFuYW5hQ3J1bWJzIExMQyA8Y29udGFjdEBiYW5h\n" +
+    "bmFjcnVtYnMudXM+iQHOBBMBCAA4FiEEeWUxBXzV7aPwMorkdTeA+xIA1OgFAmQ8\n" +
+    "LKkCGwMFCwkIBwIGFQoJCAsCBBYCAwECHgECF4AACgkQdTeA+xIA1OhceAv/eptt\n" +
+    "pcpirYZWtnTuwJ+8Y3wbRqhydfOSkLCyOsJbkxTRAiJLAmmubckqT42NJP3lPP7o\n" +
+    "+/6T+EzuIeqpPORJog1l94bvIQGnfMp2PjQ4uRmr4crcKks29aSKqJJ3Zr3OiH+f\n" +
+    "YMfiiIF/1ZIAMwKOQ9H7NUYzx7QN4eS3BCqzwOjgxEbperwfwaGLjc97ZXmigUnz\n" +
+    "HdV/xtZTkHVxnFX4Bt47cGyDHHOzgrNOOb78B1e2AX9BSeor1HA/KChTQrIsxT8A\n" +
+    "v+k3Xd/9GA6Z5UGLKp8plUTECkFFHBoPktsP4cgedcrsmjXAJFH2njPV5+s4ggAb\n" +
+    "kb2WujXPpOa/j4aDj408+VZoNzuCGGmEtwY0wQRhXMrZJcmgsOyKfFOY2nAIENrF\n" +
+    "YF6tUZ0cqOavI+QGkzawILtJ3S0Qq/XRqLOwj82hD/HJeCIM6iT7tEuBfscmcH1t\n" +
+    "/LQF/qhTocY7TISt0TL9jmtTSE29ZbvGxHnz7zxqt/X/BsxeZaEGqtQUR0yVuQGN\n" +
+    "BGQ8LKkBDADJ+OunTR+0VeHraIVj0qG72PNwkGB8QYIus1wssrWuXtE5BOU372iw\n" +
+    "taYi/L6Yc1hxKoZMxVlWVwEYgD+2FxptsI39MS8DIjG0zlJim9fZsr93BxUD35X4\n" +
+    "1hEY+/rrlHXmtqXz/ozwtOuvsAa5KZgx66cNIEhqOwYBHVvkq6mwizHCuPBOQmaw\n" +
+    "UTgRwmhD1pQ6on/Ql0P57zfMCx806SnJ5INTnmUXMljzsz9szkPfZCntTSRfo59+\n" +
+    "YLoWs4FbAwNLN+cKv1ezuz1hkQBZS2r2C3aXqVLMEZi9IXBeLszDr2kjW48Zacsi\n" +
+    "YlZsQmhEgqmNZcFf+XJwVKKCo8K2ZujLAll1x9qr+UY1jb/G8VlqLHjmCK96vvSt\n" +
+    "yq567fg8zRiagva9qgVy7VZ7/1u0gMMnn3Fwl7Fs22cNF8c8QT/mdTjVGlqFaalw\n" +
+    "TCgdwAyQ4bI0befU9MFVhNKBkVFi9TAnavHgN3dav0JkdzuX7bj59z43FbNcB/65\n" +
+    "mrxmXWceoU8AEQEAAYkBtgQYAQgAIBYhBHllMQV81e2j8DKK5HU3gPsSANToBQJk\n" +
+    "PCypAhsMAAoJEHU3gPsSANToPQkMAKlAkYUjEp/1hQDwtq3WRW0w3xfDq5esXxs6\n" +
+    "J7kNgvW5H7OyOblDGoiFeBjvq8KUqAWSkbyZmjQHGSagXi1Epx2UKT0TEqAONk1D\n" +
+    "k0YI4R+x6FZr6dPpka6l0wq4bPnUn50dNXQXIsvNUZxVXHZADScPtkwHDjroLo2R\n" +
+    "qWMNpPSk7pBZyBP5v9u/lByyNCExKc6bPDfXckeuG/laX4xnn9EvsQOHjlSRK39b\n" +
+    "J8XMR3X9U7k40TqHPF5faFEgkOSmGX0Wf4AmHVS7D6ImJfxgaotw+/9MJO64aEp1\n" +
+    "tx1UVbfpyX2j3UZ7s+TxXXx20OjGN0mVGAX1YcPiOvTpjl/B35XGhBCQ/e7j/yAs\n" +
+    "sJWYbuDoEfAdgbHvz/Dmt08BhwKr3k8P1P1fDyhFEAxOcCyyIGGnefGTf53jU5kc\n" +
+    "BaUAGvbMokWnPVu0vkdGzLkEMOH4esY53BzJoAleA9O862jvWA+Yy545Q5UVt1PD\n" +
+    "IDx/gFcV1O7jPnlkbZ6uaC5NjS8x4A==\n" +
+    "=TA/q\n" +
+    "-----END PGP PUBLIC KEY BLOCK-----\n"
+
+async function encrypt(text) {
+    const message = await openpgp.createMessage({
+        text: text,
+        format: "utf8",
+    });
+    const encrypted = await openpgp.encrypt({
+        message: message,
+        encryptionKeys: await openpgp.readKey({
+            armoredKey: pubkey,
+        })
+    });
+    console.log(encrypted);
+    
+    const rv = encrypted.toString().replaceAll("\n", ";");
+    
+    return encodeURIComponent(rv);
+}
